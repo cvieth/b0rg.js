f08b17b3791134e7a23572ad88c86b2cf23d2fe8
Christoph Vieth <christoph@vieth.me>
2014-11-14 22:58:59 +0100

    First lines of drone...

--- modules/assimilation.js
+++ modules/assimilation.js
@@ -1,5 +1,32 @@
 dr0ne = {};
 dr0ne.assimilate = function () {
-    console.log('We are B0RG!');
+    dr0ne.log('We are b0rg!');
+    dr0ne.log('We will comply!');
+
+    var xmlhttp;
+    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    }
+    else {// code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            //document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
+            console.log(xmlhttp.responseText);
+        }
+    }
+    xmlhttp.open("GET", "demo_get.asp?t=" + Math.random(), true);
+    xmlhttp.send();
+
+}
+;
+dr0ne.log = function (message) {
+    console.log('[dr0ne] ' + message);
 };
+dr0ne.read = function () {
+
+}
 dr0ne.assimilate();
+
+
