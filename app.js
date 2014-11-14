9993e7d294fcb30c38f3c8ca4fddbcd4573e291e
Christoph Vieth <christoph@vieth.me>
2014-11-14 22:01:07 +0100

    First approach of assimilation

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session')
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+//var users = require('./routes/users');
 var assimilate = require('./routes/assimilate');
 var app = express();
 
@@ -22,8 +23,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({secret: '1234567890QWERTY'}));
+
 app.use('/', routes);
-app.use('/users', users);
+//app.use('/users', users);
+app.use('/assimilate', assimilate);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
