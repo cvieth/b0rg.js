3ac78596a7ac0397544e6d977bfa1e4126f8f853
Christoph Vieth <christoph@vieth.me>
2014-11-26 23:18:28 +0100

    Added job broadcasting

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('command', {
-      numDrones: drones.data.length
-        });
+router.get('/', function (req, res) {
+
+    if (req.param('command') != undefined) {
+        drones.data.forEach(function (drone) {
+            console.log(drone);
+            var job = {
+                done: false,
+                src: req.param('command')
+            };
+            if (drone.jobs == undefined) {
+                drone.jobs = [];
+            }
+            drone.jobs.push(job);
+        })
+    }
+    console.log(req.param('name'));
+
+    ;
+    res.render('command', {
+        numDrones: drones.data.length
+    });
 });
 
-router.get('/stats', function(req, res) {
+router.get('/stats', function (req, res) {
     res.render('stats', {
         title: 'b0rg.js',
         subline: 'resistance is futile',
@@ -16,14 +33,14 @@ router.get('/stats', function(req, res) {
     });
 });
 
-router.get('/list', function(req, res) {
+router.get('/list', function (req, res) {
     console.log(drones);
     res.render('list', {
         drones: drones.data
     });
 });
 
-router.get('/details', function(req, res) {
+router.get('/details', function (req, res) {
     var currentDrone = drones.findOne({'name': req.param('name')});
     console.log(currentDrone);
     if (currentDrone.length == 0) {
